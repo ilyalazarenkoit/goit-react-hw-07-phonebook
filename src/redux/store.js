5126193dbf879b0721b7d47b013b60fbd722f484
ilyalazarenkoit <ilyalazarenkoit@gmail.com>
2023-01-03 15:09:59 +0200

    initial commit

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsSlice } from './contacts/contactsSlice';
+import { filterSlice } from './filter/filterSlice';
+
+
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsSlice.reducer,
+    filter: filterSlice.reducer,
+  },
+});
+
+
